b1da5ddbd403500e336ef7e171419d0ee6e13029
Mahesh <maheshgugulot@gmail.com>
2024-01-19 21:08:22 +0530

    created Models-chapter,course,index,page,title

---
+++ index.js
@@ -0,0 +1,8 @@
+const app = require("./app")
+const port = process.env.PORT || 3000;
+
+app.
+
+app.listen(port,()=>{
+    console.log(`started express server at port ${port}`);
+})
---
+++ models/page.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Page extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Page.init({
+    completed: DataTypes.BOOLEAN,
+    ChapterId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Page',
+  });
+  return Page;
+};
